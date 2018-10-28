6f5bbd00db00c3bfc4c24848d5649adcf9f98dbc
IMMANUEL\Immanuel <immanuelbt@gmail.com>
2018-10-28 15:43:47 +0700

    commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,51 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_message');
+		$perusahaan = $this->mymodel->getall('perusahaan');
+		print_r($perusahaan);
+		foreach ($perusahaan as $key => $value) {
+			$cek = $this->mymodel->withquery('SELECT table_name FROM information_schema.tables where table_schema="olap"
+			and table_name = "'.$value->table_name.'_div"','row');
+			if(!empty($cek)){
+				$detail = $this->mymodel->withquery('select max(Dividends) as max,min(Dividends) as min, avg(Dividends) as avg from '.$value->table_name."_div",'row');			
+				if (!empty($detail)) {
+					$data = array('id_perusahaan' => $value->id_perusahaan,
+					'min_div' =>$detail->min,'max_div' =>$detail->max, 'avg_div'=>$detail->avg,
+					'dividend'=>true);
+					$insert = $this->mymodel->insert('div_detail',$data);
+				}
+			}else {
+					
+				$data = array('id_perusahaan' => $value->id_perusahaan,
+				'min_div' =>0,'max_div' =>0, 'avg_div'=>0,
+				'dividend'=>false);
+				$insert = $this->mymodel->insert('div_detail',$data);
+			}
+		}
+	}
+
+	public function replace(){
+		$perusahaan = $this->mymodel->getall('perusahaan');
+		//print_r($perusahaan);
+		foreach ($perusahaan as $key => $value) {
+			//$cek = $this->mymodel->withquery('SELECT table_name FROM information_schema.tables where table_schema="olap"','row');			
+			$cek2 = $this->mymodel->withquery('SELECT id,tanggal FROM '.$value->table_name.' WHERE BUKA is NULL','result');
+			foreach($cek2 as $key2 => $value2){
+				$cek3 = $this->mymodel->withquery('SELECT * FROM '.$value->table_name.' WHERE id = '.(($value2->id)-1),'row');
+				print_r($cek3);
+				$data = array(
+					'Buka' => $cek3->Buka,
+					'High' => $cek3->High,
+					'Low' => $cek3->Low,
+					'Tutup' => $cek3->Tutup,
+					'Adj_Close' => $cek3->Adj_Close,
+					'Volume' => $cek3->Volume );
+				$update = $this->mymodel->update($value->table_name,$data,'id',$value2->id);			
+				//echo $value->table_name . "<br>"; //nama tabel
+				//break;
+			}
+			//break;
+		}
 	}
 }
